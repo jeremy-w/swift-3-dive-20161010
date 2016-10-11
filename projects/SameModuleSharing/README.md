3779778829647a37ba3a40d633d30c29e462d52e
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-10-11 12:56:14 -0400

    Add a test Xcodeproj for cross-language linking

---
+++ projects/SameModuleSharing/README.md
@@ -0,0 +1,5 @@
+# Same Module Sharing
+
+Used to look at what the heck Xcode does under the covers to let you link between Swift and Obj-C code.
+Made in frustration when basic attempts to turn a Foo.swiftmodule + Foo.h into something Obj-C can use didn't pan out.
+I will gain the understanding needed to dash off quick experiments at the CLI with Swift!
---
+++ projects/SameModuleSharing/SameModuleSharing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SameModuleSharing
+//
+//  Created by Jeremy on 2016-10-11.
+//  Copyright © 2016 Jeremy W. Sherman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
