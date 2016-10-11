85c316827aabdc57db246b0df76fdafb43571eb3
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-10-11 14:56:25 -0400

    Group all the exhaustiveness files

---
+++ language/exhaustiveness/my-256-case-enum-exhaustiveness.swift
@@ -0,0 +1,519 @@
+enum ManyCases{
+    case value0
+    case value1
+    case value2
+    case value3
+    case value4
+    case value5
+    case value6
+    case value7
+    case value8
+    case value9
+    case value10
+    case value11
+    case value12
+    case value13
+    case value14
+    case value15
+    case value16
+    case value17
+    case value18
+    case value19
+    case value20
+    case value21
+    case value22
+    case value23
+    case value24
+    case value25
+    case value26
+    case value27
+    case value28
+    case value29
+    case value30
+    case value31
+    case value32
+    case value33
+    case value34
+    case value35
+    case value36
+    case value37
+    case value38
+    case value39
+    case value40
+    case value41
+    case value42
+    case value43
+    case value44
+    case value45
+    case value46
+    case value47
+    case value48
+    case value49
+    case value50
+    case value51
+    case value52
+    case value53
+    case value54
+    case value55
+    case value56
+    case value57
+    case value58
+    case value59
+    case value60
+    case value61
+    case value62
+    case value63
+    case value64
+    case value65
+    case value66
+    case value67
+    case value68
+    case value69
+    case value70
+    case value71
+    case value72
+    case value73
+    case value74
+    case value75
+    case value76
+    case value77
+    case value78
+    case value79
+    case value80
+    case value81
+    case value82
+    case value83
+    case value84
+    case value85
+    case value86
+    case value87
+    case value88
+    case value89
+    case value90
+    case value91
+    case value92
+    case value93
+    case value94
+    case value95
+    case value96
+    case value97
+    case value98
+    case value99
+    case value100
+    case value101
+    case value102
+    case value103
+    case value104
+    case value105
+    case value106
+    case value107
+    case value108
+    case value109
+    case value110
+    case value111
+    case value112
+    case value113
+    case value114
+    case value115
+    case value116
+    case value117
+    case value118
+    case value119
+    case value120
+    case value121
+    case value122
+    case value123
+    case value124
+    case value125
+    case value126
+    case value127
+    case value128
+    case value129
+    case value130
+    case value131
+    case value132
+    case value133
+    case value134
+    case value135
+    case value136
+    case value137
+    case value138
+    case value139
+    case value140
+    case value141
+    case value142
+    case value143
+    case value144
+    case value145
+    case value146
+    case value147
+    case value148
+    case value149
+    case value150
+    case value151
+    case value152
+    case value153
+    case value154
+    case value155
+    case value156
+    case value157
+    case value158
+    case value159
+    case value160
+    case value161
+    case value162
+    case value163
+    case value164
+    case value165
+    case value166
+    case value167
+    case value168
+    case value169
+    case value170
+    case value171
+    case value172
+    case value173
+    case value174
+    case value175
+    case value176
+    case value177
+    case value178
+    case value179
+    case value180
+    case value181
+    case value182
+    case value183
+    case value184
+    case value185
+    case value186
+    case value187
+    case value188
+    case value189
+    case value190
+    case value191
+    case value192
+    case value193
+    case value194
+    case value195
+    case value196
+    case value197
+    case value198
+    case value199
+    case value200
+    case value201
+    case value202
+    case value203
+    case value204
+    case value205
+    case value206
+    case value207
+    case value208
+    case value209
+    case value210
+    case value211
+    case value212
+    case value213
+    case value214
+    case value215
+    case value216
+    case value217
+    case value218
+    case value219
+    case value220
+    case value221
+    case value222
+    case value223
+    case value224
+    case value225
+    case value226
+    case value227
+    case value228
+    case value229
+    case value230
+    case value231
+    case value232
+    case value233
+    case value234
+    case value235
+    case value236
+    case value237
+    case value238
+    case value239
+    case value240
+    case value241
+    case value242
+    case value243
+    case value244
+    case value245
+    case value246
+    case value247
+    case value248
+    case value249
+    case value250
+    case value251
+    case value252
+    case value253
+    case value254
+    case value255
+
+var uint8: UInt8 {
+  switch self {
+    case .value0: return 0
+    case .value1: return 1
+    case .value2: return 2
+    case .value3: return 3
+    case .value4: return 4
+    case .value5: return 5
+    case .value6: return 6
+    case .value7: return 7
+    case .value8: return 8
+    case .value9: return 9
+    case .value10: return 10
+    case .value11: return 11
+    case .value12: return 12
+    case .value13: return 13
+    case .value14: return 14
+    case .value15: return 15
+    case .value16: return 16
+    case .value17: return 17
+    case .value18: return 18
+    case .value19: return 19
+    case .value20: return 20
+    case .value21: return 21
+    case .value22: return 22
+    case .value23: return 23
+    case .value24: return 24
+    case .value25: return 25
+    case .value26: return 26
+    case .value27: return 27
+    case .value28: return 28
+    case .value29: return 29
+    case .value30: return 30
+    case .value31: return 31
+    case .value32: return 32
+    case .value33: return 33
+    case .value34: return 34
+    case .value35: return 35
+    case .value36: return 36
+    case .value37: return 37
+    case .value38: return 38
+    case .value39: return 39
+    case .value40: return 40
+    case .value41: return 41
+    case .value42: return 42
+    case .value43: return 43
+    case .value44: return 44
+    case .value45: return 45
+    case .value46: return 46
+    case .value47: return 47
+    case .value48: return 48
+    case .value49: return 49
+    case .value50: return 50
+    case .value51: return 51
+    case .value52: return 52
+    case .value53: return 53
+    case .value54: return 54
+    case .value55: return 55
+    case .value56: return 56
+    case .value57: return 57
+    case .value58: return 58
+    case .value59: return 59
+    case .value60: return 60
+    case .value61: return 61
+    case .value62: return 62
+    case .value63: return 63
+    case .value64: return 64
+    case .value65: return 65
+    case .value66: return 66
+    case .value67: return 67
+    case .value68: return 68
+    case .value69: return 69
+    case .value70: return 70
+    case .value71: return 71
+    case .value72: return 72
+    case .value73: return 73
+    case .value74: return 74
+    case .value75: return 75
+    case .value76: return 76
+    case .value77: return 77
+    case .value78: return 78
+    case .value79: return 79
+    case .value80: return 80
+    case .value81: return 81
+    case .value82: return 82
+    case .value83: return 83
+    case .value84: return 84
+    case .value85: return 85
+    case .value86: return 86
+    case .value87: return 87
+    case .value88: return 88
+    case .value89: return 89
+    case .value90: return 90
+    case .value91: return 91
+    case .value92: return 92
+    case .value93: return 93
+    case .value94: return 94
+    case .value95: return 95
+    case .value96: return 96
+    case .value97: return 97
+    case .value98: return 98
+    case .value99: return 99
+    case .value100: return 100
+    case .value101: return 101
+    case .value102: return 102
+    case .value103: return 103
+    case .value104: return 104
+    case .value105: return 105
+    case .value106: return 106
+    case .value107: return 107
+    case .value108: return 108
+    case .value109: return 109
+    case .value110: return 110
+    case .value111: return 111
+    case .value112: return 112
+    case .value113: return 113
+    case .value114: return 114
+    case .value115: return 115
+    case .value116: return 116
+    case .value117: return 117
+    case .value118: return 118
+    case .value119: return 119
+    case .value120: return 120
+    case .value121: return 121
+    case .value122: return 122
+    case .value123: return 123
+    case .value124: return 124
+    case .value125: return 125
+    case .value126: return 126
+    case .value127: return 127
+    case .value128: return 128
+    case .value129: return 129
+    case .value130: return 130
+    case .value131: return 131
+    case .value132: return 132
+    case .value133: return 133
+    case .value134: return 134
+    case .value135: return 135
+    case .value136: return 136
+    case .value137: return 137
+    case .value138: return 138
+    case .value139: return 139
+    case .value140: return 140
+    case .value141: return 141
+    case .value142: return 142
+    case .value143: return 143
+    case .value144: return 144
+    case .value145: return 145
+    case .value146: return 146
+    case .value147: return 147
+    case .value148: return 148
+    case .value149: return 149
+    case .value150: return 150
+    case .value151: return 151
+    case .value152: return 152
+    case .value153: return 153
+    case .value154: return 154
+    case .value155: return 155
+    case .value156: return 156
+    case .value157: return 157
+    case .value158: return 158
+    case .value159: return 159
+    case .value160: return 160
+    case .value161: return 161
+    case .value162: return 162
+    case .value163: return 163
+    case .value164: return 164
+    case .value165: return 165
+    case .value166: return 166
+    case .value167: return 167
+    case .value168: return 168
+    case .value169: return 169
+    case .value170: return 170
+    case .value171: return 171
+    case .value172: return 172
+    case .value173: return 173
+    case .value174: return 174
+    case .value175: return 175
+    case .value176: return 176
+    case .value177: return 177
+    case .value178: return 178
+    case .value179: return 179
+    case .value180: return 180
+    case .value181: return 181
+    case .value182: return 182
+    case .value183: return 183
+    case .value184: return 184
+    case .value185: return 185
+    case .value186: return 186
+    case .value187: return 187
+    case .value188: return 188
+    case .value189: return 189
+    case .value190: return 190
+    case .value191: return 191
+    case .value192: return 192
+    case .value193: return 193
+    case .value194: return 194
+    case .value195: return 195
+    case .value196: return 196
+    case .value197: return 197
+    case .value198: return 198
+    case .value199: return 199
+    case .value200: return 200
+    case .value201: return 201
+    case .value202: return 202
+    case .value203: return 203
+    case .value204: return 204
+    case .value205: return 205
+    case .value206: return 206
+    case .value207: return 207
+    case .value208: return 208
+    case .value209: return 209
+    case .value210: return 210
+    case .value211: return 211
+    case .value212: return 212
+    case .value213: return 213
+    case .value214: return 214
+    case .value215: return 215
+    case .value216: return 216
+    case .value217: return 217
+    case .value218: return 218
+    case .value219: return 219
+    case .value220: return 220
+    case .value221: return 221
+    case .value222: return 222
+    case .value223: return 223
+    case .value224: return 224
+    case .value225: return 225
+    case .value226: return 226
+    case .value227: return 227
+    case .value228: return 228
+    case .value229: return 229
+    case .value230: return 230
+    case .value231: return 231
+    case .value232: return 232
+    case .value233: return 233
+    case .value234: return 234
+    case .value235: return 235
+    case .value236: return 236
+    case .value237: return 237
+    case .value238: return 238
+    case .value239: return 239
+    case .value240: return 240
+    case .value241: return 241
+    case .value242: return 242
+    case .value243: return 243
+    case .value244: return 244
+    case .value245: return 245
+    case .value246: return 246
+    case .value247: return 247
+    case .value248: return 248
+    case .value249: return 249
+    case .value250: return 250
+    case .value251: return 251
+    case .value252: return 252
+    case .value253: return 253
+    case .value254: return 254
+    case .value255: return 255
+}
+}
+}
