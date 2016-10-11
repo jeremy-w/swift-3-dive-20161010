f1efe19f58be04374c10773a21bd2a341206520d
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2016-10-11 12:56:43 -0400

    Kick Xcode into creating a bridging header

---
+++ projects/SameModuleSharing/SameModuleSharing/ObjCViewController.h
@@ -0,0 +1,13 @@
+//
+//  ObjCViewController.h
+//  SameModuleSharing
+//
+//  Created by Jeremy on 2016-10-11.
+//  Copyright © 2016 Jeremy W. Sherman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ObjCViewController : UIViewController
+
+@end
